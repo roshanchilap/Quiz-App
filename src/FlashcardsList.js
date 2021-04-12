120a7e663aa0ddb482fda310d4b979d89e0fe456
roshanchilap <roshanchilap143@gmail.com>
2021-04-12 13:06:10 +0530

    Success

---
+++ src/FlashcardsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Flashcard from "./Flashcard";
+
+export default function FlashcardsList({ flashcards }) {
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <Flashcard flashcard={flashcard} key={flashcard.id} />;
+      })}
+    </div>
+  );
+}
